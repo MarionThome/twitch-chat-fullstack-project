9b48a81d75d5f655da42733570ee722e7ca88fcc
Marion <thm.marion@gmail.com>
2023-03-28 17:44:16 +0200

    remove message

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,7 @@ import ChatRoom from "./ChatRoom";
 import UserNameModal from "./UsernameModal";
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket} from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 const PUSHER_KEY = process.env.REACT_APP_PUSHER_KEY;
 const PUSHER_CLUSTER = process.env.REACT_APP_PUSHER_CLUSTER;
 
@@ -47,33 +47,49 @@ export default function Home() {
               return new Date(sec.date) + new Date(first.date);
             })
           );
+          console.log("1 =>", messageList);
         }
       });
 
     // connect to pusher (channel chat) and listens events (message)
 
     const channel = pusher.subscribe("chat");
+
     channel.bind("message", (newMessage) => {
-      console.log("New Message=>", newMessage)
+      console.log("New Message=>", newMessage);
       setMessageList((prev) => [...prev, newMessage]);
+      console.log("2 =>", messageList);
     });
 
+    // connect to pusher (channel chat) and listens events (update)
     channel.bind("messageToUpdate", (messageToUpdate) => {
-      channel.bind("messageToUpdate", (messageToUpdate) => {
-        const newList = messageList.map((e) => {
-          if (e._id === messageToUpdate.id) {
-            return {
-              ...e,
-              message: messageToUpdate.message
-            };
-          }
-          return e;
-        })
-        console.log(newList)
-        setTimeout(() => {
-          setMessageList(newList)
-       }, 200);
-    });})
+      console.log("MAP =>", messageList);
+      setMessageList((prev) => prev.map((e) => {
+        if (e._id === messageToUpdate.id) {
+          return {
+            ...e,
+            message: messageToUpdate.message,
+            edited : true
+          };
+        }
+        return e;
+      }))
+    });
+
+        // connect to pusher (channel chat) and listens events (delete) => the message is not deleted from db but only its content
+    channel.bind("messageToRemove", (messageToRemove) => {
+      console.log("MAP =>", messageList);
+      setMessageList((prev) => prev.map((e) => {
+        if (e._id === messageToRemove.id) {
+          return {
+            ...e,
+            message: messageToRemove.message,
+            deleted : true
+          };
+        }
+        return e;
+      }))
+    });
 
     // will disconect from pusher when component is unmounted
     return () => {
@@ -98,21 +114,23 @@ export default function Home() {
     }
   }, [username]);
 
+  console.log("5 =>", messageList);
+
   return (
     <main className={styles.main}>
       <div className={styles.content}>
         <div className={styles.header}>
-        <div style={{color:"white", fontSize : "14px"}}>{welcome}</div>
-           <FontAwesomeIcon
-          icon={faArrowRightFromBracket}
-          opacity="0.7"
-          cursor="pointer"
-          size="2x"
-          color="#ddf3f0"
-          onClick={() => dispatch(resetUsername())}
-        />
+          <div style={{ color: "white", fontSize: "14px" }}>{welcome}</div>
+          <FontAwesomeIcon
+            icon={faArrowRightFromBracket}
+            opacity="0.7"
+            cursor="pointer"
+            size="2x"
+            color="#ddf3f0"
+            onClick={() => dispatch(resetUsername())}
+          />
         </div>
-        
+
         {!username && <UserNameModal welcomeMessage={welcomeMessage} />}
         <ChatRoom messages={messageList} users={userList} />
       </div>
--- components/Message.js
+++ components/Message.js
@@ -1,41 +1,90 @@
-import styles from "../styles/Message.module.css"
-import moment from "moment"
-import { useState, useEffect, useRef } from "react"
+import styles from "../styles/Message.module.css";
+import moment from "moment";
+import { useState, useEffect, useRef } from "react";
 
-export default function Message(props){
-    const [toEdit, setToEdit] = useState(false);
-    const [message, setMessage] = useState(props.message)
-    const inputRef = useRef(null)
+export default function Message(props) {
+  const [toEdit, setToEdit] = useState(false);
+  const [message, setMessage] = useState(props.message);
+  const inputRef = useRef(null);
 
-    useEffect(() => {
-        if(toEdit){
-            inputRef.current.focus()
-        }
-    }, [toEdit])
+  useEffect(() => {
+    if (toEdit) {
+      inputRef.current.focus();
+    }
+  }, [toEdit]);
 
-    const editMessage = () => {
-        setToEdit(!toEdit)
-        if(message !== props.message){
-            fetch(`http://localhost:3000/update-message/${props._id}`, {
+  const editMessage = () => {
+    setToEdit(!toEdit)
+    if (message !== props.message) {
+      fetch(`http://localhost:3000/update-message/${props._id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: message }),
       })
         .then((res) => res.json())
         .then((data) => {
-         console.log(data)
+          console.log(data);
         });
-        }
     }
+  };
 
-    
-    return(
-        <div>
-            <p style={{ color: props.color, textAlign : props.isAuthor && "right"}} className={styles.author}>{props.author}</p>
-              {!toEdit && <p style={{textAlign : props.isAuthor && "right"}} className={styles.messageText}>{props.message}</p>}
-              {toEdit && <input type="text" style={{textAlign : props.isAuthor && "right"}} className={styles.editMessageInput} ref={inputRef} placeHolder={props.message} value={message} onChange={(e) => setMessage(e.target.value)}/>}
-              <p style={{textAlign : props.isAuthor && "right"}} className={styles.date}>{moment(props.date).calendar()}</p>
-              {props.isAuthor && <p onClick={()=>editMessage()}>{toEdit ? "save" : "edit"}</p>}
-        </div>
-    )
+  const removeMessage = () => {
+    fetch(`http://localhost:3000/remove-message/${props._id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: message }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  
+  const messageOptions = () => {
+    if(props.isAuthor && !props.deleted){
+      return(
+      <div>
+        <p onClick={() => editMessage()}>{toEdit ? "save" : "edit"}</p>
+        <p onClick={() => removeMessage()}>delete</p>
+      </div>
+      )
+    }
+  }
+
+  return (
+    <div>
+      <p
+        style={{ color: props.color, textAlign: props.isAuthor && "right" }}
+        className={styles.author}
+      >
+        {props.author}
+      </p>
+      {!toEdit && (
+        <p
+          style={{ textAlign: props.isAuthor && "right" }}
+          className={props.deleted ? styles.deletedText : styles.messageText}
+        >
+          {props.message}
+        </p>
+      )}
+      {toEdit && (
+        <input
+          type="text"
+          style={{ textAlign: props.isAuthor && "right" }}
+          className={styles.editMessageInput}
+          ref={inputRef}
+          placeHolder={props.message}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      )}
+      <p
+        style={{ textAlign: props.isAuthor && "right" }}
+        className={styles.date}
+      >
+        {moment(props.date).calendar()}
+      </p>
+      {messageOptions()}
+    </div>
+  );
 }
